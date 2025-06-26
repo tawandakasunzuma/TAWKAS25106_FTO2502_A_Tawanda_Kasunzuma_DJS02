dd4d4b23aa036b3c94d6a399d0c32fa4301e43e2
tawandakasunzuma <tskasunzuma@gmail.com>
2025-06-26 21:37:33 +0200

    scope and encaptualte CSS into shadow DOM

--- scripts/PodcastCard.js
+++ scripts/PodcastCard.js
@@ -29,31 +29,139 @@ export class PodcastCard extends HTMLElement {
         const genreSpans = podcastData.genres
             .map((id,index,array) => {
                 const genreName = genreMap.get(id);
-                const separator = index < array.length - 1 ? ", " : ""; // Add comma and space to each genre except the last one
-                return `<span class="genre-name">${genreName}</span>${separator}`
+                return `<span class="genre-name">${genreName}</span>`
             })
             .join("");
         this.shadowRoot.innerHTML = `
-            <style></style>
-            <article class="podcast-container">
-                <div class="image-container">
-                    <img
-                    class="podcast-image"
-                    src="${podcastData.image}"
-                    alt="${podcastData.description}"
-                    />
-                    <p class="podcast-cover-title">${podcastData.title}</p>
-                </div>
-                <h3 class="podcast-title">${podcastData.title}</h3>
-                <div class="seasons-section">
-                    <img class="icon seasons-icon" src="./images/seasons-icon.svg" alt="Seasons" />
-                    <p class="num-seasons">${podcastData.seasons} seasons</p>
-                </div>
-                <div class="genre-section">
-                    ${genreSpans}
-                </div>
-                <p class="updated-status">Updated ${updatedDate}</p>
-            </article>
+            <style>
+                /* Podcast container */
+
+                .podcast-container {
+                    margin-top: 1rem;
+                    background-color: var(--white-color);
+                    padding: var(--normal-padding);
+                    border-radius: var(--normal-border-radius);
+                    box-shadow: 0 2px 4px var(--light-gray-color);
+                    width: 100%;
+                    transition: all 300ms ease-in-out;
+                }
+
+                .podcast-container:hover,
+                .podcast-container:active {
+                    box-shadow: 0 4px 8px var(--medium-gray-color);
+                    transform: translateY(-2px);
+                }
+
+                /* Podcast image */
+
+                .image-container {
+                    width: 100%;
+                    max-width: 100%;
+                }
+
+                .podcast-image {
+                    height: auto;
+                    width: 100%;
+                    border-radius: var(--normal-border-radius);
+                    cursor: pointer;
+                    object-fit: cover;
+                }
+
+                /* Podcast title */
+
+                .podcast-title {
+                    font-size: 1rem;
+                    cursor: pointer;
+                }
+
+                /* Seasons section */
+
+                .seasons-section {
+                    display: flex;
+                    align-items: center;
+                    gap: var(--small-gap);
+                    margin: 0.5rem 0;
+                }
+
+                .seasons-icon {
+                    width: 20px;
+                    cursor: auto;
+                }
+
+                .num-seasons {
+                    font-size: 0.75rem;
+                    font-weight: 600;
+                }
+
+                /* Genre section */
+
+                .genre-section {
+                    display: flex;
+                    flex-wrap: wrap;
+                    gap: 0.5rem var(--small-gap);
+                    margin: 0.5rem 0;
+                }
+
+                .genre-name {
+                    background-color: var(--light-gray-color);
+                    font-size: 0.75rem;
+                    font-weight: 600;
+                    padding: 0.125rem 0.5rem;
+                    border-radius: var(--small-border-radius);
+                    cursor: pointer;
+                    white-space: nowrap;
+                    transition: all 300ms ease-in-out;
+                }
+
+                .genre-name:hover {
+                    background-color: rgba(128, 128, 128, 0.075);
+                }
+
+                /* Updated Status */
+
+                .updated-status {
+                    font-size: 0.75rem;
+                    font-weight: 600;
+                    color: var(--dark-gray-color);
+                    margin-top: 1rem;
+                }
+                
+                /*===============
+                    Media Quires
+                ===============*/
+
+                @media (max-width: 480px) {
+                    .podcast-cover-title {
+                        height: 94%;
+                    }
+                }
+
+                @media (min-width: 481px) and (max-width: 767px) {
+                    .podcast-cover-title {
+                        height: 94%;
+                    }
+                }
+
+            </style>
+            
+                <article class="podcast-container">
+                    <div class="image-container">
+                        <img
+                        class="podcast-image"
+                        src="${podcastData.image}"
+                        alt="${podcastData.description}"
+                        />
+                    </div>
+                    <h3 class="podcast-title">${podcastData.title}</h3>
+                    <div class="seasons-section">
+                        <img class="icon seasons-icon" src="./images/seasons-icon.svg" alt="Seasons" />
+                        <p class="num-seasons">${podcastData.seasons} seasons</p>
+                    </div>
+                    <div class="genre-section">
+                        ${genreSpans}
+                    </div>
+                    <p class="updated-status">Updated ${updatedDate}</p>
+                </article>
         `;
     }
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,11 @@
 import './PodcastCard.js';
-import { podcasts } from './data.js';
+import { podcasts } from '../scripts/data.js';
 
-const cardSection = document.getElementById('card-section');
-
-podcasts.forEach (podcast => {
-    const card = document.createElement("podcast-card");
+document.addEventListener('DOMContentLoaded', () => {
+  const cardSection = document.getElementById('card-section');
+  podcasts.forEach(podcast => {
+    const card = document.createElement('podcast-card');
     card.podcastData = podcast;
     cardSection.append(card);
-})
-
-console.log("Rendered",podcasts.length,"cards");
+  });
+});
