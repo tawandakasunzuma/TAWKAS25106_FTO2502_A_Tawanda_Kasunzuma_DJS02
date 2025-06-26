e6143113c82e9d4c3ae7f755fd2e0248c5b7f0b7
tawandakasunzuma <tskasunzuma@gmail.com>
2025-06-26 20:10:22 +0200

    create logic for date and genre and add it to shadowRoot in PodcastCard.js file

--- scripts/PodcastCard.js
+++ scripts/PodcastCard.js
@@ -1,3 +1,5 @@
+import { genres } from '../scripts/data.js';
+
 export class PodcastCard extends HTMLElement {
     constructor () {
         super ();
@@ -12,10 +14,46 @@ export class PodcastCard extends HTMLElement {
     }
 
     render () {
+
+        const podcastData = this._data;
+        
+        // Days since last update
+        const date = new Date (podcastData.updated);
+        const day = String(date.getDate()).padStart(2, "0"); // Get day of month
+        const month = date.toLocaleString("en", { month: "long" }); // Get the fill month
+        const year = date.getFullYear(); // Get full year
+        const updatedDate = `${day} ${month} ${year}`
+        
+        // Genre titles
+        const genreMap = new Map (genres.map(genre => [genre.id, genre.title]));
+        const genreSpans = podcastData.genres
+            .map((id,index,array) => {
+                const genreName = genreMap.get(id);
+                const separator = index < array.length - 1 ? ", " : ""; // Add comma and space to each genre except the last one
+                return `<span class="genre-name">${genreName}</span>${separator}`
+            })
+            .join("");
         this.shadowRoot.innerHTML = `
-            <div>
-                ${JSON.stringify(this._data)}
-            </div>
+            <style></style>
+            <article class="podcast-container">
+                <div class="image-container">
+                    <img
+                    class="podcast-image"
+                    src="${podcastData.image}"
+                    alt="${podcastData.description}"
+                    />
+                    <p class="podcast-cover-title">${podcastData.title}</p>
+                </div>
+                <h3 class="podcast-title">${podcastData.title}</h3>
+                <div class="seasons-section">
+                    <img class="icon seasons-icon" src="./images/seasons-icon.svg" alt="Seasons" />
+                    <p class="num-seasons">${podcastData.seasons} seasons</p>
+                </div>
+                <div class="genre-section">
+                    ${genreSpans}
+                </div>
+                <p class="updated-status">Updated ${updatedDate}</p>
+            </article>
         `;
     }
 
